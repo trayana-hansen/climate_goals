734a25c4e5cfcabc907be9e0dff8dcc86b0dc78d
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-01-06 13:52:29 +0100

    last commit 06/01

--- climate_goals/src/App.jsx
+++ climate_goals/src/App.jsx
@@ -1,11 +1,10 @@
 import "./App.scss";
+import "./Components/Partials/Footer/footer.module.scss";
+import "./Components/Partials/Header/header.module.scss";
+import "./Components/Partials/Main/main.module.scss";
 
 const App = () => {
-  return (
-    <div className="App">
-
-    </div>
-  );
-}
+  return <div className="App"></div>;
+};
 
 export default App;
