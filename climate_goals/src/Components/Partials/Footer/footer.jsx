a2e2958c51f5cbbdd3ed788d7b14654aa2221476
Trayana Hansen <92487819+trayana-hansen@users.noreply.github.com>
2023-01-11 12:28:55 +0100

    styling

--- climate_goals/src/Components/Partials/Footer/footer.jsx
+++ climate_goals/src/Components/Partials/Footer/footer.jsx
@@ -1,33 +1,39 @@
 import logo from "../../../images/KT-Logo.png";
+import styles from "./footer.module.scss";
 
 const Footer = () => {
   return (
-    <footer className="footermain">
-      <div className="footer-info">
-        <h3>KlimaTossen NPO</h3>
+    <footer className={styles.footerMain}>
+      <div className={styles.footerInfo}>
+        <h4>KlimaTossen NPO</h4>
         <p>Vesterbrogade 92</p>
         <p>9000 Aalborg</p>
         <p>info@klima-tossen.dk</p>
         <p> 9922 3344 (Åben man - fre: 08:00 - 16:00)</p>
-        <h3>SAMARBEJDSPARTNERE:</h3>
-        <a href="https://www.dn.dk">https://www.dn.dk</a>
+        <h4>SAMARBEJDSPARTNERE:</h4>
+        <a href="https://www.dn.dk">https://www.dn.dk</a> <br />
         <a href="https://www.miljoportal.dk">https://www.miljoportal.dk</a>
+        <div className={styles.footerImg}>
+          <img src={logo} alt="KT-Logo" />
+        </div>
       </div>
-      <div className="footer-form">
-        <h3>Ja tak! Jeg vil gerne høre mere.</h3>
+      <div className={styles.footerForm}>
+        <h4>Ja tak! Jeg vil gerne høre mere.</h4>
         <form>
           <input type="text" placeholder="Fulde navn" />
           <input type="text" placeholder="Hjem by" />
           <input type="email" placeholder="Email adresse" />
           <input type="number" placeholder="Telefon nummer" />
-          <label htmlFor="checkone">Få en samtale med Klimatossen</label>
-          <input name="checkone" type="checkbox" />
-          <label htmlFor="checkwto">Modtag Nyhedsbrev</label>
-          <input name="checktwo" type="checkbox" />
+          <label><input type="checkbox" />Få en samtale med Klimatossen</label>
+
+          <label>
+            <input type="checkbox" />
+            Modtag Nyhedsbrev
+          </label>
+
           <button>Send</button>
         </form>
       </div>
-      <img src={logo} alt="KT-Logo" />
     </footer>
   );
 };
--- climate_goals/src/Components/Partials/Main/main.jsx
+++ climate_goals/src/Components/Partials/Main/main.jsx
@@ -7,7 +7,6 @@ import img4 from "../../../images/Icon-Arrangement.png";
 import spacer1 from "../../../images/Spacer-01.png";
 import spacer2 from "../../../images/Spacer-02.png";
 import spacer3 from "../../../images/Spacer-03.png";
-import spacer4 from "../../../images/Spacer-04.png";
 
 const Main = () => {
   return (
@@ -15,7 +14,7 @@ const Main = () => {
       <Section>
         <article className={styles.sectionOne}>
           <h2>HVAD ER CO2?</h2>
-          <figure className="figure-main">
+          <figure className={styles.figureMain}>
             <img src={img1} alt="co2-img" />
             <figcaption>
               <h3>
@@ -45,108 +44,111 @@ const Main = () => {
               </a>
             </figcaption>
           </figure>
-          <img className="divider" src={spacer1} alt="divider-one" />
         </article>
+        <img className={styles.divider} src={spacer1} alt="divider-one" />
       </Section>
 
       <Section>
         <article className={styles.sectionTwo}>
-		<h2>KLIMA OG MILJØ?</h2>
-        <figure className="figure-main">
-          <img src={img2} alt="climate-icon" />
-          <figcaption>
-            <h3>Kort om klimaet og miljøet?</h3>
-            <p>- Klima og miljø hænger sammen.</p>
-            <p>- Klimaforandringer påvirker miljøet og omvendt.</p>
-            <p>
-              - Miljøet kan betragtes som en organisme, og er det vi baserer
-              vores liv og overlevelse på, hvis vi ændrer i miljøet, har det
-              oftest en dominoeffekt, der påvirker alle dele i miljøet.
-            </p>
-            <p>
-              - Ændringer i atmosfærens CO2 og Ozonlag påvirker miljøet på hele
-              kloden, med ændringer i temperatur og nedbør som følge.
-            </p>
-            <p>
-              - Ændrede temperaturer og nedbørsmønster påvirker ikke kun kældre
-              og infrastruktur, men også biodiversiteten som planter insekter
-              mm.
-            </p>
-            <p>- Din adfærd påvirker klimaet i størrer eller mindre grad.</p>
-            <a href="læsmere" className="more-info">
-              Læs Mere
-            </a>
-          </figcaption>
-        </figure>
-        <img className="divider" src={spacer2} alt="divider-two" />
-		</article>
+          <h2>KLIMA OG MILJØ?</h2>
+          <figure className={styles.figureMain}>
+            <img src={img2} alt="climate-icon" />
+            <figcaption>
+              <h3>Kort om klimaet og miljøet?</h3>
+              <p>- Klima og miljø hænger sammen.</p>
+              <p>- Klimaforandringer påvirker miljøet og omvendt.</p>
+              <p>
+                - Miljøet kan betragtes som en organisme, og er det vi baserer
+                vores liv og overlevelse på, hvis vi ændrer i miljøet, har det
+                oftest en dominoeffekt, der påvirker alle dele i miljøet.
+              </p>
+              <p>
+                - Ændringer i atmosfærens CO2 og Ozonlag påvirker miljøet på
+                hele kloden, med ændringer i temperatur og nedbør som følge.
+              </p>
+              <p>
+                - Ændrede temperaturer og nedbørsmønster påvirker ikke kun
+                kældre og infrastruktur, men også biodiversiteten som planter
+                insekter mm.
+              </p>
+              <p>- Din adfærd påvirker klimaet i størrer eller mindre grad.</p>
+              <a href="læsmere" className="more-info">
+                Læs Mere
+              </a>
+            </figcaption>
+          </figure>
+        </article>
+        <img className={styles.divider} src={spacer2} alt="divider-two" />
       </Section>
       <Section>
         <article className={styles.sectionThree}>
-		<h2>HVORDAN KAN JEG BIDRAGE?</h2>
-        <figure className="figure-main">
-          <img src={img3} alt="icon-checklist" />
-          <figcaption>
-            <h3>Hvordan belaster du miljøet mindst muligt i dagligdagen?</h3>
-            <p>
-              - Dit forbrug overordnet herunder din produktion af affald,
-              bidrager til udledning af CO2
-            </p>
-            <p>
-              - Fødevareproduktion har stor indflydelse på CO2 balancen, tænk
-              over dit valg i dagligdagen.
-            </p>
-            <p>
-              - Din bolig og den måde du har indrettet dig påvirker miljøet.
-            </p>
-            <p>
-              - Jordens skove og verdenshavene er den største "producent" af den
-              ilt, der er nødvendig, for liv på Jorden, som vi kender det.
-            </p>
-            <p>
-              - Mennesket er den enlige faktor der har den største indflydelse
-              på CO2 -balancen på Jorden i dag!
-            </p>
-            <p>
-              - Du kan påvirke CO2 balancen, både ved at ændre små ting i din
-              hverdag, eller ved aktivt deltage i kampen for et bedre miljø.
-            </p>
-            <a href="læsmere" className="more-info">
-              Læs Mere
-            </a>
-          </figcaption>
-        </figure>
-        <img className="divider" src={spacer3} alt="divider-three" />
-		</article>
+          <h2>HVORDAN KAN JEG BIDRAGE?</h2>
+          <figure className={styles.figureMain}>
+            <img src={img3} alt="icon-checklist" />
+            <figcaption>
+              <h3>Hvordan belaster du miljøet mindst muligt i dagligdagen?</h3>
+              <p>
+                - Dit forbrug overordnet herunder din produktion af affald,
+                bidrager til udledning af CO2
+              </p>
+              <p>
+                - Fødevareproduktion har stor indflydelse på CO2 balancen, tænk
+                over dit valg i dagligdagen.
+              </p>
+              <p>
+                - Din bolig og den måde du har indrettet dig påvirker miljøet.
+              </p>
+              <p>
+                - Jordens skove og verdenshavene er den største "producent" af
+                den ilt, der er nødvendig, for liv på Jorden, som vi kender det.
+              </p>
+              <p>
+                - Mennesket er den enlige faktor der har den største indflydelse
+                på CO2 -balancen på Jorden i dag!
+              </p>
+              <p>
+                - Du kan påvirke CO2 balancen, både ved at ændre små ting i din
+                hverdag, eller ved aktivt deltage i kampen for et bedre miljø.
+              </p>
+              <a href="læsmere" className="more-info">
+                Læs Mere
+              </a>
+            </figcaption>
+          </figure>
+        </article>
+        <img className={styles.divider} src={spacer3} alt="divider-three" />
       </Section>
       <Section>
         <article className={styles.sectionFour}>
-		<h2>DELTAG I ARRANGEMENT</h2>
-        <figure className="figure-main">
-          <img src={img4} alt="icon-cleaning" />
-          <figcaption>
-            <h3>Deltag i et af vores arrangementer og gør en forskel!</h3>
-            <p>
-              - “Combing” (”at fjerne affald”, f.eks. beachcombing: , rense
-              stranden for affald og elementer der ikke hører hjemme her).
-              Invester en dag med familien i naturen og sammen med andre.
-            </p>
-            <p>- Tilmeld dig eet arrangement i dit område</p>
-            <p>- Tag ansvar hjælp dit nærområde og os alle sammen generelt.</p>
-            <p>- Mød nye mennesker omkring en fælles opgave.</p>
-            <p>- Tag famillien med på en dag for naturen og vores fremtid..</p>
-            <p>
-              - Tilmeld dig eet af vore arrangementer, eller modtag information
-              om Klimatossen!
-            </p>
-            <p>- Hent mere information på siden “Tilmeld Dig”.</p>
-            <a href="læsmere" className="more-info">
-              Læs Mere
-            </a>
-          </figcaption>
-        </figure>
-        <img className="divider" src={spacer4} alt="divider-four" />
-		</article>
+          <h2>DELTAG I ARRANGEMENT</h2>
+          <figure className={styles.figureMain}>
+            <img src={img4} alt="icon-cleaning" />
+            <figcaption>
+              <h3>Deltag i et af vores arrangementer og gør en forskel!</h3>
+              <p>
+                - “Combing” (”at fjerne affald”, f.eks. beachcombing: , rense
+                stranden for affald og elementer der ikke hører hjemme her).
+                Invester en dag med familien i naturen og sammen med andre.
+              </p>
+              <p>- Tilmeld dig eet arrangement i dit område</p>
+              <p>
+                - Tag ansvar hjælp dit nærområde og os alle sammen generelt.
+              </p>
+              <p>- Mød nye mennesker omkring en fælles opgave.</p>
+              <p>
+                - Tag famillien med på en dag for naturen og vores fremtid..
+              </p>
+              <p>
+                - Tilmeld dig eet af vore arrangementer, eller modtag
+                information om Klimatossen!
+              </p>
+              <p>- Hent mere information på siden “Tilmeld Dig”.</p>
+              <a href="læsmere" className="more-info">
+                Læs Mere
+              </a>
+            </figcaption>
+          </figure>
+        </article>
       </Section>
     </>
   );
